d627428d07a7e3f3455f9efb877281803fbc805e
IIvanov29 <4Ivani51@solent.ac.uk>
2019-10-03 13:17:53 +0000

    W/e

---
+++ 1-basics/3-decision/1-if/bot.py
@@ -0,0 +1,10 @@
+#Helping Beep decide whaet book is he reading
+
+print("What type of book is this?")
+booktype=input()
+
+if(booktype == "adventure"):
+    print("I like adventure books!")
+
+print("Finished reading book.")
+
