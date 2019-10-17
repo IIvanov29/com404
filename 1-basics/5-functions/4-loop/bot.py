fb1c0d5bdde01c1a2922099f398118811e52b5d4
IIvanov29 <4Ivani51@solent.ac.uk>
2019-10-17 13:44:26 +0000

    w/e

---
+++ 1-basics/5-functions/4-loop/bot.py
@@ -0,0 +1,12 @@
+#Loop with a function
+
+def cross_bridge(distance):
+    for count in range (0,distance, 1):
+        print("Crossed step")
+    if distance > 5:
+        print("The bridge is collapsing!")
+    else:
+        print("we must keep going")
+
+cross_bridge(6)
+cross_bridge(3)
